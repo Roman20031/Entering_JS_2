193f482cf2f0c2b6290c0cba4eba823ad5b009b3
Roman <rdemyanenko10@gmail.com>
2024-09-29 00:44:44 +0300

    Entering_JS_2

--- JS/script.js
+++ JS/script.js
@@ -11,5 +11,11 @@ function clearDisplay(){
 }
 
 function calculate(){
-    display.value = eval(display.value);
+    try{
+        display.value = eval(display.value);
+    }
+    catch(error){
+        display.value = "Error" 
+    }
+    
 }
